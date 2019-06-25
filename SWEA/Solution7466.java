de326ea4ecb0989693456250dee455e41480eb70
sitaround <sitaround91@gmail.com>
2019-06-25 14:33:10 +0900

    solve 7466, 7733

---
+++ SWEA/Solution7466.java
@@ -0,0 +1,45 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+//문제의 테스트케이스가 너무 적음. 시간 초과가 나는 N, K 값을 넣어도 통과됨. 
+//테스트케이스가 더 추가되면 제대로 풀어서 올리겠음.
+class Solution7466{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        int answer;
+        int N, K;
+        
+        for(int test_case = 1; test_case <= T; test_case++){
+            bw.write("#"+test_case+" ");
+            answer = 1;
+            String[] parse = br.readLine().trim().split(" ");
+            N = Integer.parseInt(parse[0]);
+            K = Integer.parseInt(parse[1]);
+            if(N < K){
+                while(N > 0){
+                    if(K == 1) break;
+                    if(K % N == 0){
+                        answer *= N;
+                        K /= N;
+                        if(N >= K) N = K;
+                        else N--;
+                    }else N--;
+                }
+            }else{
+                answer = K;
+            }
+            bw.write(answer+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
---
+++ SWEA/Solution7733.java
@@ -0,0 +1,87 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+//좀 더 최적화를 해서 올릴 계획
+class Solution7733{
+    static final int[] dx = {-1, 1, 0, 0}; //상 하 좌 우
+    static final int[] dy = {0, 0, -1, 1};
+    static int[][] cheese;
+    static int N;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int tmp;
+        int max;
+        int answer;
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            bw.write("#"+test_case+" ");
+            answer = 0; max = 0;
+            N = Integer.parseInt(br.readLine());
+            cheese = new int[N+2][N+2];
+            for(int i = 1; i <= N; i++){
+                String[] parse = br.readLine().trim().split(" ");
+                for(int j = 1; j <= N; j++){
+                    tmp = Integer.parseInt(parse[j-1]);
+                    cheese[i][j] = tmp;
+                    if(tmp > max)
+                        max = tmp;
+                }
+            }
+            for(int days = 1; days < max; days++){
+                for(int i = 1; i <= N; i++){
+                    for(int j = 1; j <= N; j++){
+                        cheese[i][j]--;
+                    }
+                }
+                tmp = bfs();
+                if(tmp > answer) answer = tmp;
+            }
+            if(answer == 0) answer++;
+            bw.write(answer+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static int bfs(){
+        int cnt = 1;
+        int[][] arr = new int[N+2][N+2];
+        boolean[][] visit = new boolean[N][N];
+        Queue<Integer> x = new LinkedList<>();
+        Queue<Integer> y = new LinkedList<>();
+        int tmpX, tmpY;
+        int idxX, idxY;
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j <= N; j++){
+                if(cheese[i][j] > 0 && arr[i][j] == 0){
+                    x.add(i); y.add(j);
+                    arr[i][j] = cnt;
+                    while(!x.isEmpty()){
+                        tmpX = x.poll(); tmpY = y.poll();
+                        for(int k = 0; k < 4; k++){
+                            idxX = tmpX + dx[k];
+                            idxY = tmpY + dy[k];
+                            if(cheese[idxX][idxY] > 0 && !visit[idxX-1][idxY-1]){
+                                x.add(idxX); y.add(idxY);
+                                arr[idxX][idxY] = cnt;
+                                visit[idxX-1][idxY-1] = true;
+                            }
+                        }
+                    }
+                    cnt++;
+                }
+            }
+        }
+        return cnt-1;
+    }
+}
