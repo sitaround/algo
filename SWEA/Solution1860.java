46bc36325f519495ce33f0ec9de9f07702df74f1
sitaround <sitaround91@gmail.com>
2020-05-07 16:38:37 +0900

    optimize 1860

--- SWEA/Solution1860.java
+++ SWEA/Solution1860.java
@@ -30,19 +30,50 @@ public class Solution1860 {
         br.close();
     }
 
+    // static String solve(int n, int m, int k, int[] arr){
+    //     int max;
+    //     Arrays.sort(arr);
+    //     max = arr[n-1];
+    //     int[] num = new int[max+1];
+    //     for(int i = 1; i < max+1; i++){
+    //         num[i] = (i/m)*k;
+    //     }
+    //     for(int i = 0; i < n; i++){
+    //         for(int j = arr[i]; j < max+1; j++){
+    //             num[j]--;
+    //             if(num[j] < 0) return "Impossible";
+    //         }
+    //     }
+    //     return "Possible";
+    // }
+
+    // 실행시간 줄이기
+    // arr: n명이 도착한 시간
+    // m초동안 k개 생산
     static String solve(int n, int m, int k, int[] arr){
-        int max;
+        int num = 0;
+        int s = 0;
+        int tmp;
         Arrays.sort(arr);
-        max = arr[n-1];
-        int[] num = new int[max+1];
-        for(int i = 1; i < max+1; i++){
-            num[i] = (i/m)*k;
+        if(arr[0] >= m){
+            tmp = arr[0] / m;
+            num = tmp * k;
+            s = tmp * m;
         }
-        for(int i = 0; i < n; i++){
-            for(int j = arr[i]; j < max+1; j++){
-                num[j]--;
-                if(num[j] < 0) return "Impossible";
+        num--;
+        if(num < 0) return "Impossible";
+        System.out.println(num + ", " + s);
+        for(int i = 1; i < n; i++){
+            if(arr[i-1] == arr[i]) num--;
+            else{
+                if(arr[i] >= s){
+                    tmp = (arr[i] - s) / m;
+                    s += m * tmp;
+                    num += tmp * k;
+                }
+                num--;
             }
+            if(num < 0) return "Impossible";
         }
         return "Possible";
     }
